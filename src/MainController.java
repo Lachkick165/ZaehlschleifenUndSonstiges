e221c29095c8ff84d7f2af4796d198f5433c8ad2
Lachkick165 <LachTwitch@gmail.com>
2023-04-18 22:49:27 +0200

    Initial commit

---
+++ src/MainController.java
@@ -0,0 +1,17 @@
+public class MainController {
+    public static void main(String[] args) {
+        printSomeNumbers();
+        printNumbers(1, 1);
+    }
+
+    public static void printSomeNumbers(){
+        for (int i = 6; i <= 23; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void printNumbers(int start, int end){
+      
+    }
+}
